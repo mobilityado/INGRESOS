c3a41f340765f23cc5dacdc9c612e100f56b8832
mobilityado <jose.lopez.vencedor@gmail.com>
2026-07-13 14:45:32 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Pega aquí la URL /exec de tu implementación de Google Apps Script.
 window.APP_CONFIG = {
-  API_URL: "PEGA_AQUI_TU_URL_DE_APPS_SCRIPT_EXEC",
+  API_URL: "https://docs.google.com/spreadsheets/d/1t7_19QrIufcoX-osGVlm4sZ4fpGt4ljP26wwg8nh-Tc/edit?usp=sharing",
   DEFAULT_SHEET: "TRTVB"
 };
