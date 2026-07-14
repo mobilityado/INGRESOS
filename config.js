4f959b848b8a808525d8f6fee966f671a1f326be
mobilityado <jose.lopez.vencedor@gmail.com>
2026-07-13 21:09:37 -0600

    Update API_URL to the new Google Apps Script URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   // URL de la aplicación web de Google Apps Script terminada en /exec
-  API_URL: "PEGA_AQUI_LA_URL_DE_APPS_SCRIPT_EXEC"
+  API_URL: "https://script.google.com/macros/s/AKfycbxC2mKnafSrn3ngqrzjOqxWcH2Ueh7DEIge0cweju3PHTrRJ7CS5iqQczMpAiWcjLaZIA/exec"
 };
